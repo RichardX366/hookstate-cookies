8215f8c896d0f8ca36b227acaa2e40e4f2cb0c34
Richard X <richardx366@gmail.com>
2022-09-04 23:06:32 -0400

    init

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { Plugin } from '@hookstate/core';
+import { parse, serialize } from 'cookie';
+
+const pluginId = Symbol('hookstate-cookies');
+const CookiePersistence = (key: string) => (): Plugin => ({
+  id: pluginId,
+  init: (state) => {
+    const persisted = parse(document.cookie)[key];
+    if (persisted) {
+      state.set(JSON.parse(persisted));
+    } else if (!state.promised && !state.error) {
+      document.cookie = serialize(key, JSON.stringify(state.value), {
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365 * 100),
+      });
+    }
+    return {
+      onSet: function (p) {
+        if ('state' in p) {
+          document.cookie = serialize(key, JSON.stringify(state.value), {
+            expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365 * 100),
+          });
+        } else {
+          document.cookie = serialize(key, '', {
+            expires: new Date(0),
+          });
+        }
+      },
+    };
+  },
+});
+
+export default CookiePersistence;
